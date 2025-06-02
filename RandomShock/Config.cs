1839b19952bfe9ba54df64e0d7c22923ed03c3a6
Mara Kalwa <62842654+marakalwa@users.noreply.github.com>
2025-06-02 03:12:38 +0200

    Add project files.

---
+++ RandomShock/Config.cs
@@ -0,0 +1,90 @@
+﻿using OpenShock.SDK.CSharp.Models;
+using Tomlyn;
+
+namespace RandomShock
+{
+    internal class Config
+    {
+        private static readonly Random _random = new();
+        public string ApiToken { get; set; }
+
+        public int CheckInterval { get; set; }
+
+        public Dictionary<string, ShockerConfig> Shocker { get; set; }
+        public Dictionary<string, ActionDetails> Action { get; set; }
+
+        public class ShockerConfig
+        {
+            public string Uuid { get; set; }
+        }
+
+        public class ActionDetails
+        {
+            public ControlType Type { get; set; }
+            public byte Intensity { get; set; }
+            public float Duration { get; set; }
+            public double Weight { get; set; }
+
+            public string[]? Shockers { get; set; }
+        }
+
+        private double totalWeight;
+
+        public static Config Load(string filePath)
+        {
+            var toml = File.ReadAllText(filePath);
+            var model = Toml.ToModel<Config>(toml);
+
+            model.ApiToken = Environment.GetEnvironmentVariable("OPENSHOCK_TOKEN") ?? model.ApiToken;
+
+            if (model.Shocker.Count == 0) throw new Exception("No shockers provided!");
+
+            if (model.Action.Count == 0) throw new Exception("No actions provided!");
+
+            model.totalWeight = model.Action.Values.Sum(a => a.Weight);
+
+            return model;
+        }
+
+        public ControlRequest CreateRandomControlRequest()
+        {
+            var randomWeight = _random.NextDouble() * totalWeight;
+
+            ActionDetails? selectedAction = null;
+            var cumulativeWeight = 0.0;
+            foreach (var action in Action.Values)
+            {
+                cumulativeWeight += action.Weight;
+                if (randomWeight <= cumulativeWeight)
+                {
+                    selectedAction = action;
+                    break;
+                }
+            }
+
+            var shockers = selectedAction.Shockers?
+                .Select(shocker => Shocker.GetValueOrDefault(shocker))
+                .Where(shocker => shocker != null)
+                .ToArray();
+
+            if (shockers == null || !shockers.Any())
+            {
+                var shockerIndex = _random.Next(Shocker.Count);
+                shockers = [Shocker.Values.ElementAt(shockerIndex)];
+            }
+
+            return new ControlRequest
+            {
+                Shocks = shockers.Select(shocker =>
+                    new Control
+                    {
+                        Id = Guid.Parse(shocker.Uuid),
+                        Type = selectedAction.Type,
+                        Intensity = selectedAction.Intensity,
+                        Duration = (ushort)(selectedAction.Duration * 1000)
+                    }),
+                CustomName = $"DeathDetect - Shock"
+            };
+        }
+    }
+}
---
+++ RandomShock/Main.cs
@@ -0,0 +1,61 @@
+﻿using NLog;
+using OpenShock.SDK.CSharp;
+using System.Diagnostics;
+
+namespace RandomShock
+{
+    /// <summary>
+    /// Provides an easy way to configure and send random shocks using the OpenShock API.
+    /// </summary>
+    public class RandomShock
+    {
+        private bool detection = false;
+
+        /// <summary>
+        /// Starts the Random Shock service, which continuously checks for detection and sends control requests.
+        /// </summary>
+        public async Task Start()
+        {
+            var logger = LogManager.GetCurrentClassLogger();
+
+            var config = Config.Load("config.toml");
+
+            var client = new OpenShockApiClient(new ApiClientOptions
+            {
+                Token = config.ApiToken
+            });
+
+            const int period = 500;
+            var lastDetection = false;
+
+            logger.Info("Random Shock initialized");
+            while (true)
+            {
+                var stopwatch = Stopwatch.StartNew();
+
+                if (detection)
+                {
+                    logger.Info("Positive detected, sending control request");
+                    var controlRequest = config.CreateRandomControlRequest();
+                    await client.ControlShocker(controlRequest)
+                        .ContinueWith(task => logger.Info($"Control request status: {task.Result}"));
+                }
+
+                lastDetection = detection;
+
+                stopwatch.Stop();
+                var elapsed = stopwatch.ElapsedMilliseconds;
+                var delay = Math.Max(0, period - elapsed);
+                Thread.Sleep((int)delay);
+            }
+        }
+        /// <summary>
+        /// Sets the detection state for the Random Shock service.
+        /// </summary>
+        /// <param name="value">Wether a detection was made</param>
+        public void SetDetection(bool value)
+        {
+            detection = value;
+        }
+    }
+}
